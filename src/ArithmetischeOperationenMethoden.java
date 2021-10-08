f812bc7b2679fb453ea82ba4b3ad772340c78870
pputz <putz.patrick@outlook.com>
2021-10-08 21:26:50 +0200

    added methods for arithmetic operations

---
+++ src/ArithmetischeOperationenMethoden.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ArithmetischeOperationenMethoden {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        Scanner getData = new Scanner(System.in);
+
+        System.out.print("Declare variable a: ");
+        a = getData.nextInt();
+        System.out.print("Declare variable b: ");
+        b = getData.nextInt();
+
+        printResult(addition(a, b));
+        printResult(subtraction(a, b));
+        printResult(multiplication(a, b));
+        printResult(division(a, b));
+        printResult(modulo(a, b));
+    }
+
+    public static void printResult(int result){
+        System.out.println(result);
+    }
+    public static int addition(int a, int b){
+        return a + b;
+    }
+    public static int subtraction(int a, int b){
+        return a - b;
+    }
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+    public static int division(int a, int b){
+        return a / b;
+    }
+    public static int modulo(int a, int b){
+        return a % b;
+    }
+
+    public static void AddAndPrint(int a, int b){
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
--- src/Calculator.java
+++ src/Calculator.java
@@ -25,5 +25,34 @@ public class Calculator {
         
         float fThree = fOne / fTwo;  // Division von integer werten mit float ergebnis: (a * 1.0) / (b * 1.0)
         System.out.println("fThree = " + fThree);
+
+        int iA = 3;
+        int iR = iA++;
+        System.out.println("iR = " + iR);
+        System.out.println("iA = " + iA);
+
+        String stringOne = "Alpha";
+        String stringTwo = "Alpha";
+        System.out.println(stringOne.equals(stringTwo));
+
+        boolean bBool = !(2 == 2 || 3 == 4);
+        System.out.println(bBool);
+
+        printResult(2 + 3);
+        int wub = 2 + 4;
+        printResult(wub);
+
+
+
+        int result = add(5,25);
+        printResult(result);
+    }
+
+    public static void printResult(int result){
+        System.out.println(result);
+    }
+
+    public static int add(int a, int b){
+        return a + b;
     }
 }
