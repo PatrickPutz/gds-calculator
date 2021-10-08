bc050d3527a080b9f21aafce097f2d1c1a692ff0
pputz <putz.patrick@outlook.com>
2021-10-08 19:59:03 +0200

    declared and printed variables

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,14 @@
 public class Calculator {
     public static void main(String[] args) {
-
+        int a = 27;
+        System.out.println("a = " + a);
+        double b = 17.5;
+        System.out.println("b = " + b);
+        boolean c = false;
+        System.out.println("c = " + c);
+        char d = 'Y';
+        System.out.println("d = " + d);
+        String e = "Bamboozled";
+        System.out.println("e = " + e);
     }
 }
