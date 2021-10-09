54c5d753ebba780a1535b26a34e2d8649c0c7546
pputz <putz.patrick@outlook.com>
2021-10-09 12:02:56 +0200

    implemented arithmetic operations of two arrays

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -12,5 +12,32 @@ public class ArrayDemo {
             sum = sum + articles[i];
         }
         System.out.println("Gesamtkosten: " + sum);
+
+        int[] ArrayOne = new int [3];
+        ArrayOne[0] = 3;
+        ArrayOne[1] = 4;
+        ArrayOne[2] = 5;
+
+        int[] ArrayTwo = new int [3];
+        ArrayTwo[0] = 7;
+        ArrayTwo[1] = 1;
+        ArrayTwo[2] = 3;
+
+        for(int i = 0; i < ArrayOne.length; i++){
+            System.out.println(ArrayOne[i] + " + " + ArrayTwo[i] + " = " + (ArrayOne[i] + ArrayTwo[i]));
+        }
+        for(int i = 0; i < ArrayOne.length; i++){
+            System.out.println(ArrayOne[i] + " - " + ArrayTwo[i] + " = " + (ArrayOne[i] - ArrayTwo[i]));
+        }
+        for(int i = 0; i < ArrayOne.length; i++){
+            System.out.println(ArrayOne[i] + " * " + ArrayTwo[i] + " = " + (ArrayOne[i] * ArrayTwo[i]));
+        }
+        for(int i = 0; i < ArrayOne.length; i++){
+            System.out.println(ArrayOne[i] + " / " + ArrayTwo[i] + " = " + (ArrayOne[i] / ArrayTwo[i]));
+        }
+        for(int i = 0; i < ArrayOne.length; i++){
+            System.out.println(ArrayOne[i] + " % " + ArrayTwo[i] + " = " + (ArrayOne[i] % ArrayTwo[i]));
+        }
+
     }
 }
